c327c48d3ecb2594d29b90f2aeaec30654f95525
MITHUN KUMAR <mithunkumar039@gmail.com>
2018-03-28 15:04:39 +0530

    Update server.R

--- speciality_analysis/server.R
+++ speciality_analysis/server.R
@@ -3,18 +3,11 @@ library(dplyr)
 library(xts)
 library(dygraphs)
 speciality_wise<-readRDS("speciality_wise.RDA")
-city_wise<-readRDS("city_wise.rda")
 speciality_wise$X<-NULL
 speciality_wise$X.1<-NULL
 speciality_wise[is.na(speciality_wise)]<-0
-city_wise[is.na(city_wise)]<-0
 df1<-melt(speciality_wise,id.vars = c("Date","Speciality"))
-df2<-melt(city_wise,id.vars = c("Date","City_Name"))
 df1$Date<-as.Date(df1$Date, format="%m/%d/%Y")
-df2$Date<-as.Date(df2$Date, format="%m/%d/%Y")
-df2$City_Name<-as.factor(df2$City_Name)
-df2$Category<-as.factor(df2$Category)
-
 
 
 shinyServer(
@@ -24,10 +17,7 @@ shinyServer(
         group_by(Date) %>%
         summarise(n = value)})
     
-    selected2 <- reactive({df2 %>% 
-        filter(City_Name==input$City,variable == input$Type) %>% 
-        group_by(Date) %>%
-        summarise(n = value)})
+    
     
     output$title <- renderText({
       paste0(input$Type, " in ",input$Speciality)})
@@ -41,11 +31,7 @@ shinyServer(
               }
       )
     
-    output$dygraph2<-renderDygraph({
-      spe_xts2<-xts(selected2()$n,order.by = as.Date(selected2()$Date))
-      dygraph(spe_xts2,xlab = "week (plot for city_wise)",ylab = "Frequency")%>%
-        dySeries("V1",label=input$Type,color="red",fillGraph = F, strokeWidth = 3, drawPoints = T, pointSize = 6)
-    })
+   
   }
 )
     
